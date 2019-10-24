f6ed1771660032c764ed516a6f1096a9307071b2
yukanchan <56749485+yukanchan@users.noreply.github.com>
2019-10-24 15:48:51 +0900

    4th

---
+++ Assets/Scripts/shiina/PazzleNodeParent2.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PazzleNodeParent2 : MonoBehaviour
+{
+    int lastNode = 3;
+
+    [SerializeField] List<GameObject> correctOrder;
+
+    [SerializeField] List<GameObject> removeThese;
+
+    [SerializeField] Vector3 removeVelocity;
+  
+
+    public void ReceiveNode(GameObject _nextNode)
+    {
+        if (correctOrder.Count > 0)
+        {
+            if (_nextNode == correctOrder[0])
+            {
+                print("found " + _nextNode.name);
+
+                correctOrder.RemoveAt(0);
+            }
+        }
+        else
+        {
+            print("all found");
+            for (int i = 0; i < removeThese.Count; i++)
+            {
+         
+
+                removeThese[i].AddComponent<PuzzleResultBehavior>();
+                removeThese[i].GetComponent<PuzzleResultBehavior>().myVelocity = removeVelocity;
+              Destroy(removeThese[i], 20);
+            }
+
+            GameObject successSoundObj = transform.Find("SuccessSound").gameObject;
+            successSoundObj.transform.parent = null;
+            successSoundObj.GetComponent<AudioSource>().Play();
+            Destroy(successSoundObj, 7);
+
+
+
+            Destroy(this.gameObject);
+        }
+
+
+
+    }
+
+
+}
