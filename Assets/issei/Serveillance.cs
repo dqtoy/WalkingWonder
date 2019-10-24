a82f8dbb7fe74cafbd268321e36acca633bd7fc9
isseeeeei <56749460+isseeeeei@users.noreply.github.com>
2019-10-25 05:14:45 +0900

    maybe last upload

---
+++ Assets/issei/Serveillance.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Serveillance : MonoBehaviour
+{
+	void Start()
+	{
+		//transform.localScale = new Vector3(-1, 1, 1);
+	}
+
+	void Update()
+	{
+		transform.LookAt(Camera.main.transform);
+	}
+}
